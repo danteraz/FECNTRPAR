d5c3dbd36e381cef4b128979f510536dd6d09000
danteraz <danteraz@gmail.com>
2024-09-21 20:17:57 -0300

    Atualizar Para VERCEL E SUPABASE V1 21/09/24

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,22 +1,28 @@
-const { query } = require('../../../../becntrpar/config/db');
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { usuario, senha } = req.body;
 
     try {
-      const result = await query(
-        'SELECT * FROM administradores WHERE usuario = ? AND senha = ?',
-        [usuario, senha]
-      );
+      // Verifica se o usuário e a senha são válidos
+      const { data, error } = await supabase
+        .from('administradores')
+        .select('*')
+        .eq('usuario', usuario)  
+        .eq('Senha', senha);
 
-      if (result.length > 0) {
+      if (error) throw error;
+
+      if (data.length > 0) {
         res.status(200).json({ success: true });
       } else {
         res.status(401).json({ error: 'Usuário e/ou Senha Não Encontrado' });
       }
     } catch (error) {
-      console.error(error);
+      console.error('Erro ao autenticar usuário:', error);
       res.status(500).json({ error: 'Erro ao autenticar usuário' });
     }
   } else {
